e07abc292e374b35ab8714581ece42c4de3b223b
Neliswa084 <neliswangcobo141@gmail.com>
2026-07-15 15:38:17 +0200

    added the picture for hero section

--- group_ui_work/src/App.tsx
+++ group_ui_work/src/App.tsx
@@ -4,13 +4,16 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import { Navbar } from './components/Navbar/Navbar'
+import { Hero } from './components/Hero/hero'
 
 function App() {
  
 
   return (
     <>
+    
     <Navbar/>
+    <Hero/>
 
    
 </>
